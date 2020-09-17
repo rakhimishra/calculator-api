a41848d44d9c64e53e444f97e3e3937c282dcdd3
rakhimishra <mishrarakhi21@gmail.com>
2020-09-17 13:20:20 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
-const port = 3000
 app.use(express.urlencoded());
+const AppError = require("./ErrorController/appError");
+const userRouter = require("./route");
+const globalErrorHandler=require('./ErrorController/globalError');
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
-
-
 app.use(bodyParser.urlencoded({ extended: false }))
-
 app.use(bodyParser.json())
 // your code goes here
 
-// here
+//Handling base route
+app.use("", userRouter);
+
+// Handling unexpcted routes
+app.all("*", (req, res, next) => {
+  next( new AppError(`can't find ${req.originalUrl} on this server`, 404));
+});
+app.use(globalErrorHandler.globalError);
+
+const port = process.env.PORT || 9000;
+app.listen(port, () => {
+  console.log("App is running on port number ", port);
+});
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-module.exports = app;
+// module.exports = app;
